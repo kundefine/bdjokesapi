c86e87603ad59fff692f0d1933b7867a7f9bf4f0
kundefine <kundefine@gmail.com>
2019-11-04 21:31:36 +0600

    Initialize commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const fs = require('fs')
+const bodyParser = require('body-parser')
+
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/',  (req, res) => {
+    let data = fs.readFileSync('./bdjokes.json')
+    res.send(data)
+    res.end();
+})
+
+
+
+
+
+app.listen(3000, () => console.log('listening'))
